ab08f6f2f50580edf09f11831956e2628eccf569
fjsanchezm-11 <fjsanchezm09@educarex.es>
2025-04-29 10:05:06 +0200

    estilo main

--- AcceX/Frontend/src/app/components/main/main.component.ts
+++ AcceX/Frontend/src/app/components/main/main.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
