f60af07972ed6306f189e2687c0aa645719d8e80
diglesiasb04 <diglesiasb04@educarex.es>
2025-06-04 12:28:28 +0200

    aplicacion definitiva 2

--- AcceX/Frontend/src/app/services/usuario.service.ts
+++ AcceX/Frontend/src/app/services/usuario.service.ts
@@ -18,9 +18,9 @@ export class UsuarioService {
     return this.http.post<any>(this.apiUrl, usuario);
   }
 
-  obtenerInvestigadorDeUsuario(uid: number) {
-    return this.http.get<any>(`${this.apiUrl}/usuarios/${uid}/investigador`);
-  } 
+  obtenerInvestigadorDeUsuario(uid: number): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/usuarios/${uid}/investigador`);
+  }
 
   actualizarUsuario(id: number, usuario: any): Observable<any>{
     return this.http.put<any>(`${this.apiUrl}/${id}`, usuario);
